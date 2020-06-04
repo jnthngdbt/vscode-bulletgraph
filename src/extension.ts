99ca731bcb8618740e190d65dcef4a62221a6710
Jonathan Godbout <jgodbout@bathfitter.com>
2020-06-04 16:47:42 -0400

    Added render on save

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,25 @@ import { DotFileManager } from './DotFileManager'
 import { LineManager } from './LineManager'
 import { insertNodeIdStringFromLineContent } from './NodeIdGenerator'
 
+function render() {
+    // Parsing the editor file to get the bullet graph structure.
+    let bullet = new Bullet();
+    bullet.parseEditorFile();
+
+    // Simplify the graph, if necessary, by having a maximum depth.
+    let depthManager = new DepthManager();
+    let depthBullet = depthManager.pruneAndReorganize(bullet);
+
+    // Render a Graphviz dot file.
+    let dotFileManager = new DotFileManager();
+    dotFileManager.render(depthBullet);
+}
+
 export function activate(context: vscode.ExtensionContext) {
     let lineManager = new LineManager();
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('vscode-bulletgraph.renderPreview', () => {
-            // Parsing the editor file to get the bullet graph structure.
-            let bullet = new Bullet();
-            bullet.parseEditorFile();
-
-            // Simplify the graph, if necessary, by having a maximum depth.
-            let depthManager = new DepthManager();
-            let depthBullet = depthManager.pruneAndReorganize(bullet);
-
-            // Render a Graphviz dot file.
-            let dotFileManager = new DotFileManager();
-            dotFileManager.render(depthBullet);
-        })
+        vscode.commands.registerCommand('vscode-bulletgraph.renderPreview', render)
     );
 
     context.subscriptions.push(
@@ -41,6 +43,13 @@ export function activate(context: vscode.ExtensionContext) {
             lineManager.unfoldLine();
         })
     );
+
+    context.subscriptions.push(
+        vscode.workspace.onDidSaveTextDocument((doc: vscode.TextDocument) => {
+            if (doc.fileName.endsWith(".blt"))
+                render();
+        })
+    )
 }
 
 export function deactivate() {}
